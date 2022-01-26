02da6f03478bb03e9063e941b413725dce1d1383
skyvell <tskyvell@gmail.com>
2022-01-26 14:38:21 +0100

    Cleaned up in directory.

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-goloop rpc sendtx deploy ./liquidation_strategies/build/libs/liquidation_strategies-0.1.0-optimized.jar \
-    --uri https://berlin.net.solidwallet.io/api/v3 \
-    --key_store ./arbitx.json --key_password test \
-    --nid 0x7 --step_limit 3000000000 \
-    --content_type application/java \
-    --param name=contract_34
--- deploy_to_local.sh
+++
@@ -1,6 +0,0 @@
-goloop rpc sendtx deploy ./balanced-liquidation/build/libs/balanced-liquidation-0.1.0-optimized.jar \
-    --uri http://localhost:9082/api/v3 \
-    --key_store ./godWallet.json --key_password gochain \
-    --nid 3 --step_limit 3000000000 \
-    --content_type application/java \
-    --param name=contract_34
